94742fc89aadecc4a4730fd88cd1c9e14e9f9801
wangpen <931075365@qq.com>
2017-06-17 16:16:20 +0800

    [wangpeng]

---
+++ app/src/pages/report.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Topbar from '../components/Topbar'
+import { Title } from '../components/Title'
+
+class Report extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div>
+                <Topbar />
+                { Title("子导航二") }
+            </div>
+		)
+	}
+}
+export default Report;
